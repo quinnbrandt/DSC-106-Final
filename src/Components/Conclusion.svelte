6d966eac191c92b9739d007dc6fa4efd16d128d4
Quinn Brandt <family@Brandts-MacBook-Air.local>
2024-05-28 14:19:27 -0700

    pages

--- src/Components/Conclusion.svelte
+++ src/Components/Conclusion.svelte
@@ -3,8 +3,7 @@
     <h2 class="centered" style="margin-bottom: 15px">What we have done so far:</h2>
       <p class="body-text" style="margin-bottom: 30px">
         So far, we have created the webpage, implemented the scrollyteller, and established
-       some structure to the webpage. We have cleaned the data so that we can retrieve statistics for each combination of
-<<<<<<< HEAD
+        some structure to the webpage. We have cleaned the data so that we can retrieve statistics for each combination of
         exit velocity and launch angle. We created the baseball field, and added sliders below the baseball diamond so that you can
         change the exit velocity and launch angle. We included a brief introduction to the project, along
         with our overall objectives.
@@ -16,19 +15,6 @@
         our Jupyter Notebook to the interactive visualization. Our goal is to create text boxes when you hover over a base which
         show the number of singles, doubles, triples, or home runs for the combination of exit velocity and launch angle. We also
         want to color encode the bases so that it represents the likelihood of a hit getting on that particular base. This will be difficult as we will need to calculate coordinates, and figure out how to change the statistics based on the hovered base.
-=======
-        exit velocity and launch angle. We created the baseball field, and added sliders below the baseball diamond so that you can 
-        change the exit velocity and launch angle. We included a brief introduction to the project, along
-        with our overall objectives. 
-      </p>
-    
-    <h2 class="centered" style="margin-bottom: 15px">What will be the most challenging:</h2>
-      <p class='body-text' style="margin-bottom: 30px">
-        The most challenging part of our design will probably be connecting the data from 
-        our Jupyter Notebook to the interactive visualization. Our goal is to create text boxes when you hover over a base which
-        show the number of singles, doubles, triples, or home runs for the combination of exit velocity and launch angle. We also 
-        want to color encode the bases so that it represents the likelihood of a hit getting on that particular base.
->>>>>>> 3c96030d308dcd64095cc7b85509db554f01690f
     </p>
 </section>
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,17 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+  // preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/quinnbrandt/DSC-106-Final/' : ''
+    }
+  }
+};
